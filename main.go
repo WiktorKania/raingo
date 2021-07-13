c016a154e86fd7c3769f9459638c8851a87770e0
Wiktor Kania <wiktor.kania@htdevelopers.com>
2021-07-13 19:15:04 +0200

    connected bot to server

--- main.go
+++ main.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
+func handleMessage(session *discordgo.Session, msg *discordgo.MessageCreate) {
+	if msg.Author.ID == session.State.User.ID {
+		return
+	}
+	fmt.Println("Got a message, ", msg.Content)
+}
+
 func main() {
-	fmt.Println("Hello there", discordgo.APIVersion)
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("Error loading .env file!")
+	}
+	bot, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+	fmt.Println("API version:", discordgo.APIVersion)
+	if err != nil {
+		fmt.Println("Error creating bot session!")
+		panic(err)
+	}
+	bot.AddHandler(handleMessage)
+	bot.Open()
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+	bot.Close()
 }
